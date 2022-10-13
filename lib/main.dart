224529d07a042cae9ed4c77eec605bf9ba6d9cff
FelixJes <felixbelix007@gmail.com>
2022-10-13 14:43:28 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './providers/products.dart';
 
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
